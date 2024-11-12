2d9e2b8402a100cf0b732852a7394550bdbdd8bb
samueltorres-io <torresfernandessamuel@gmail.com>
2024-11-11 22:57:17 -0300

    Desenvolvimento da logica da aplicacao backend e consumo de api

---
+++ script.js
@@ -0,0 +1,5 @@
+const number = document.querySelector('#number');
+const street = document.querySelector('#street');
+const city = document.querySelector('#city');
+const country = document.querySelector('#country');
+
