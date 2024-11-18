fc29d9b37f969a3789f7149303ae53faca78fbb6
samueltorres-io <torresfernandessamuel@gmail.com>
2024-11-18 18:10:13 -0300

    Atualizando feature para main

--- script.js
+++
@@ -1,5 +0,0 @@
-const number = document.querySelector('#number');
-const street = document.querySelector('#street');
-const city = document.querySelector('#city');
-const country = document.querySelector('#country');
-
